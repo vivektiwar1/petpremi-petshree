4197b0b8623f1033a63c6d30c8e00d7e9b7a8fa5
rahul <malhotra.rah@gmail.com>
2020-10-02 13:40:58 +0530

    small fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  apiBase: 'http://boltonte.com:8083',
+  apiBase: 'https://petshree.com:8083',
   password: 'malhotra123',
   production: true,
   username: 'admin',
-  api: 'http://boltonte.com:8083/',
+  api: 'https://petshree.com:8083/',
   auth: btoa('admin:malhotra123'),
-  fbId: '',
-  googleId: '',
+  fbId: '2423717734604642',
+  googleId: '69629454397-m76vmhhs1ikn1cc75ncfhedksf189j0f.apps.googleusercontent.com',
 };
