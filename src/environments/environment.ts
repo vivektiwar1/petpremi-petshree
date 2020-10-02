de08e1cec776d217e53bea7f14ebb347561b4060
Vivek Tiwari <nktiwarippp@gmail.com>
2020-10-02 15:48:14 +0530

    fix api url and default loader removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   password: 'malhotra123',
   production: false,
   username: 'admin',
-  api: 'http://localhost/',
+  api: 'http://boltonte.com:8083/',
   auth: btoa('admin:malhotra123'),
   fbId: '643712706077783',
   googleId: '69629454397-m76vmhhs1ikn1cc75ncfhedksf189j0f.apps.googleusercontent.com',
