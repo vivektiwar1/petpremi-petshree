9aeadf9c958ca4cf5c29c6ab6b84188b9cf3a206
Aniket Trivedi <aniket@bloohash.com>
2020-10-24 14:16:48 +0530

    various fixes

--- src/app/modules/feature/e-card/components/appointmenttime/appointmenttime.component.ts
+++ src/app/modules/feature/e-card/components/appointmenttime/appointmenttime.component.ts
@@ -13,39 +13,50 @@ export class AppointmenttimeComponent implements OnInit {
   month: string[] = [];
   monthName: any;
   dayName: any;
-  Date:any;
-  Time:any;
+  Date: any;
+  Time: any;
   currentDate: any;
   @Input() userDetails: any;
   AddDateTime: FormGroup;
-  private time:FormControl
+
+  startDate:any;
+  aryDates:any;
+  private time: FormControl
   constructor(
     private formBuilder: FormBuilder,
   ) {
     this.date = new Date();
+
     this.month = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"
     ];
     this.dayName = this.date.getDay();
+
     this.currentDate = this.date.getUTCDate();
     this.monthName = this.month[this.date.getMonth()]
 
-    this.AddDateTime= this.formBuilder.group({
+    this.AddDateTime = this.formBuilder.group({
       date: ['', Validators.required],
       time: ['', Validators.required],
 
     })
     this.AddDateTime.get('date').valueChanges.subscribe(data => {
-      this.Date=data
+      this.Date = data
     })
     this.AddDateTime.get('time').valueChanges.subscribe(data => {
-      this.Time=data
+      this.Time = data
     })
-   
+
+    this.startDate = new Date();
+ this.aryDates = this.GetDates(this.startDate, 4);
+console.log(this.aryDates);
+
+
   }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
+    
   }
   showDiv() {
     document.getElementById('appointmenttime').style.display = "none";
@@ -63,6 +74,50 @@ export class AppointmenttimeComponent implements OnInit {
       document.getElementById('videos').style.display = "block";
     }
     document.getElementById('enquiry').style.display = "block";
+  }
+
+  GetDates(startDate, daysToAdd) {
+    var aryDates = [];
+
+    for (var i = 0; i <= daysToAdd; i++) {
+      var currentDate = new Date();
+      currentDate.setDate(startDate.getDate() + i);
+      aryDates.push({"Day":this.DayAsString(currentDate.getDay()) , "Date" : currentDate.getDate() });
+    }
+
+    return aryDates;
+  }
+
+  MonthAsString(monthIndex) {
+    var d = new Date();
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    return month[monthIndex];
+  }
+
+  DayAsString(dayIndex) {
+    var weekdays = new Array(7);
+    weekdays[0] = "Sunday";
+    weekdays[1] = "Monday";
+    weekdays[2] = "Tuesday";
+    weekdays[3] = "Wednesday";
+    weekdays[4] = "Thursday";
+    weekdays[5] = "Friday";
+    weekdays[6] = "Saturday";
+
+    return weekdays[dayIndex];
   }
 
 }
