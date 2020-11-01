66e242e9c8c0aae7416b5dc46163e687a43beaec
rahul <malhotra.rah@gmail.com>
2020-11-01 19:32:28 +0530

    lots of changes

--- src/app/modules/feature/e-card/e-card-routing.module.ts
+++ src/app/modules/feature/e-card/e-card-routing.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ClientDetailsComponent } from '../e-card/components/client-details/client-details.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ClientDetailsComponent} from '../e-card/components/client-details/client-details.component';
 
-import { ECardComponent } from './container/e-card.component';
+import {ECardComponent} from './container/e-card.component';
 
 
 const routes: Routes = [
-  { path: 'clients/:id', component: ClientDetailsComponent },
-  { path: ':userName',
-  children: [
-    { path: ':partnerUserName', component: ECardComponent}
-  ]},
-  
-  { path: '', component: ECardComponent },
+  {path: ':userName', component: ECardComponent},
+  {path: 'clients/:id', component: ClientDetailsComponent},
+  {
+    path: ':userName',
+    children: [
+      {path: ':partnerUserName', component: ECardComponent}
+    ]
+  }
+  ,
+
+  {path: '', component: ECardComponent},
 
 ];
 
