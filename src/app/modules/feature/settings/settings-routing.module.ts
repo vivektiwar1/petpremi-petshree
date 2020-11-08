e3fdf79c34945cb603ace60f654a534a87322b8b
rahul <malhotra.rah@gmail.com>
2020-11-08 21:31:45 +0530

    issue resolution.

--- src/app/modules/feature/settings/settings-routing.module.ts
+++ src/app/modules/feature/settings/settings-routing.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { ProfileComponent } from './profile/profile.component';
-import { SettingsComponent } from './settings.component';
-import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
-import { PartnerComponent } from './partner/partner.component';
-import { TimingComponent } from './timing/timing.component';
-import { PayoutComponent } from './payout/payout.component';
+import {ProfileComponent} from './profile/profile.component';
+import {SettingsComponent} from './settings.component';
+import {CustomerProfileComponent} from './customer-profile/customer-profile.component';
+import {PartnerComponent} from './partner/partner.component';
+import {TimingComponent} from './timing/timing.component';
 
 const routes: Routes = [
   {
@@ -18,7 +17,6 @@ const routes: Routes = [
       { path: 'customer-profile', component: CustomerProfileComponent },
       { path: 'partner', component: PartnerComponent },
       { path: 'timings', component: TimingComponent },
-      { path: 'payout', component: PayoutComponent }
     ]
   }
 ];
